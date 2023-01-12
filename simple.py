cc6f5e9d059eaaa9fbb9e272aaf629b82cc1905d
gsr142 <76536836+gsr142@users.noreply.github.com>
2023-01-12 14:08:41 -0800

    added day and time to video

--- simple.py
+++ simple.py
@@ -1,18 +1,24 @@
 import cv2
 import streamlit as st
-
+from datetime import datetime
 st.title("Motion Detector")
 start = st.button("Start Camera")
-
+now = datetime.now()
+day = now.strftime("%A")
+dt_string = now.strftime("%H:%M:%S")
 if start:
     streamlit_image = st.image([])
     camera = cv2.VideoCapture(0)
 
     while True:
         check, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RBG)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        cv2.putText(img=frame, text="Hello", org=(50,50),
+        cv2.putText(img=frame, text=day, org=(10, 30),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                    color=(250, 100, 200), thickness=2, lineType=cv2.LINE_AA)
+
+        cv2.putText(img=frame, text=dt_string, org=(10,70),
                     fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
                     color=(20, 100, 200), thickness=2, lineType=cv2.LINE_AA)
 
